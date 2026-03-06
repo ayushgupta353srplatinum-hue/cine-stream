1ef591715782e1877bce7ba5cb4efe10c5d6f241
Ayush Gupta <ayushgupta353srplatinum@gmail.com>
2026-03-06 11:06:40 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Favorites from "./pages/Favorites"
+import Navbar from "./components/Navbar"
+
+export default function App(){
+
+return(
+
+<BrowserRouter>
+
+<Navbar/>
+
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+<Route path="/favorites" element={<Favorites/>}/>
+
+</Routes>
+
+</BrowserRouter>
+
+)
+
+}
